38ad97094e3698fff3abd51b1fa22cf86858380d
Dmitry Orlov <feanarokurufinve@mail.ru>
2017-09-17 01:51:12 +0300

    Face elements detection

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-import numpy as np
 import cv2
+import numpy as np
+import imutils
+import dlib
+from imutils import face_utils
 
 cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('./face_predictor.dat')
+
+old_shapes = []
+counter = 1
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
-
-    # Our operations on the frame come here
-    # frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
+    # frame = imutils.resize(frame, width=200)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist(gray)
-    cascade_fn = './haarcascade_frontalface_alt.xml'
-    cascade = cv2.CascadeClassifier(cascade_fn)
 
-    rects = cascade.detectMultiScale(frame, scaleFactor=1.3, minNeighbors=4, minSize=(20, 20), flags=cv2.CASCADE_SCALE_IMAGE)
-    if len(rects):
-        rects[:,2:] += rects[:,:2]
-        for x1, y1, x2, y2 in rects:
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (255,0,123), 2)
-            cv2.imshow('camera', frame)
+    if counter % 4 == 0:
+        old_shapes = []
+        rects = detector(gray, 1)
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+            frame = face_utils.visualize_facial_landmarks(frame, shape)
+            old_shapes.append(shape)
     else:
-        # Display the resulting frame
-        cv2.imshow('camera', frame)
+        for shape in old_shapes:
+            frame = face_utils.visualize_facial_landmarks(frame, shape)
+    cv2.imshow("camera", frame)
+    counter += 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
